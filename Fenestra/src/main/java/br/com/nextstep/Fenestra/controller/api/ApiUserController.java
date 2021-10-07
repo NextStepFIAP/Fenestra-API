26f33b29975fa329d25cc52117c11aa4efaad2cd
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-10-07 18:28:37 -0300

    CrossOrigin

--- Fenestra/src/main/java/br/com/nextstep/Fenestra/controller/api/ApiUserController.java
+++ Fenestra/src/main/java/br/com/nextstep/Fenestra/controller/api/ApiUserController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import br.com.nextstep.Fenestra.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class ApiUserController {
 
 	@Autowired
